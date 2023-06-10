ef0adba35a0be39091c5ae7907ac00d0ff683d1c
egirl <egirl@debian-BULLSEYE-live-builder-AMD64>
2023-06-10 17:42:03 -0400

    aoeu

--- diffeq.m
+++ diffeq.m
@@ -6,6 +6,7 @@ ode= diff(y, x) + (2/x)*y == exp(x^3)
   
 % a solution can include the inital condition if you add it as the second arg
 ysolution(x)= dsolve(ode, y(1) == 1)
+chr= latex(ysolution(x))
 X= linspace(1, 1000);
 Y= (ysolution(X));
 plot(X, Y);
