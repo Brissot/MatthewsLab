ae57e0495b7fda7665484582ef8e97e3516576e3
egirl <egirl@debian-BULLSEYE-live-builder-AMD64>
2023-06-10 17:39:04 -0400

    Graphing doesn't work

--- diffeq.m
+++ diffeq.m
@@ -5,4 +5,7 @@ syms y(x);
 ode= diff(y, x) + (2/x)*y == exp(x^3)
   
 % a solution can include the inital condition if you add it as the second arg
-ysolution(x)= dsolve(ode)
+ysolution(x)= dsolve(ode, y(1) == 1)
+X= linspace(1, 1000);
+Y= (ysolution(X));
+plot(X, Y);
